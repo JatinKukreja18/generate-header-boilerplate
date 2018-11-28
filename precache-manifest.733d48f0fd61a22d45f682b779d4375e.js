1afce110df49ff3fc9545c359b6d5b8e856078cb
jatin kukreja <jatinkukreja18@yahoo.in>
2018-11-28 17:43:59 +0530

    Updates

---
+++ precache-manifest.733d48f0fd61a22d45f682b779d4375e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/generate-header-boilerplate/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9ef77c04f70e1ce76f69",
+    "url": "/generate-header-boilerplate/static/js/runtime~main.9ef77c04.js"
+  },
+  {
+    "revision": "a1a25d3b4eb2cb019335",
+    "url": "/generate-header-boilerplate/static/js/main.a1a25d3b.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/generate-header-boilerplate/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "a1a25d3b4eb2cb019335",
+    "url": "/generate-header-boilerplate/static/css/main.5c32724b.chunk.css"
+  },
+  {
+    "revision": "2126bba3f296c27358ec5e5ab4a4e481",
+    "url": "/generate-header-boilerplate/index.html"
+  }
+];
